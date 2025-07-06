9bed67841f6107620af1c26534faf7ac55ad1114
ExaQuiz <ganir210@gmail.com>
2025-07-06 08:55:00 -0700

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,2 +1,17 @@
 @echo off
-echo Running gradlew.bat
+
+:: Gradle startup script for Windows
+
+set DIR=%~dp0
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIR%
+
+set WRAPPER_JAR=%APP_HOME%gradle\wrapper\gradle-wrapper.jar
+
+if not exist "%WRAPPER_JAR%" (
+  echo The Gradle wrapper jar file could not be found: %WRAPPER_JAR%
+  exit /b 1
+)
+
+java -jar "%WRAPPER_JAR%" %*
+
