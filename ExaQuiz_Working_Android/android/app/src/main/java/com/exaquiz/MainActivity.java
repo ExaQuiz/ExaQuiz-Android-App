c91ffd4f28c89d811620ff176c01a3c1028ba2ae
ExaQuiz <ganir210@gmail.com>
2025-07-08 08:47:28 -0700

    ExaQuiz_Working_Android/gradlew

---
+++ ExaQuiz_Working_Android/android/app/src/main/java/com/exaquiz/MainActivity.java
@@ -0,0 +1,12 @@
+
+package com.exaquiz;
+
+import android.os.Bundle;
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+    @Override
+    protected String getMainComponentName() {
+        return "ExaQuiz";
+    }
+}
