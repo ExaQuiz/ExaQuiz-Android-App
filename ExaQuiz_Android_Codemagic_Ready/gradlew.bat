094ca07b82dc53d7dbe8d83bfb5c395206a4edcf
Loustion Merdha <ganir210@gmail.com>
2025-07-25 03:17:09 +0530

    Update gradlew.bat

--- ExaQuiz_Android_Codemagic_Ready/gradlew.bat
+++ ExaQuiz_Android_Codemagic_Ready/gradlew.bat
@@ -1,3 +1,10 @@
 @echo off
-SET JAVA_HOME="%JAVA_HOME%"
-gradlew.bat wrapper
+setlocal
+set DIR=%~dp0
+set JAVA_EXE=%JAVA_HOME%\\bin\\java.exe
+if exist "%JAVA_EXE%" (
+  "%JAVA_EXE%" -classpath "%DIR%gradle\\wrapper\\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
+) else (
+  echo ERROR: JAVA_HOME is not set correctly or Java is missing.
+  exit /b 1
+)
